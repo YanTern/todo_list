ca10037f89d67a1812ae6ad448a8feef85d3b75b
George <e.shegolihin2010@yandex.ru>
2026-03-10 22:42:34 +0300

    func definitions

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 import json
 
-# with open("tasks.json", "r") as file:
-#     tasks = json.load(file)
+with open("tasks.json", "r") as file:
+    tasks = json.load(file)
 
-# tasks.append({"title": "Make a project", "is_done": False})
 
-# with open("tasks.json", "w") as file:
-#     json.dump(tasks, file, indent=4)
+def show_tasks():
+    "Показывает задачи в списке"
 
+    pass
+
+
+def create_task():
+    "Создает новую задачу"
+
+    new_task = input("Введите новую задачу: ")
+    tasks.append({"title": new_task, "is_done": False})
+
+    with open("tasks.json", "w") as file:
+        json.dump(tasks, file, indent=4)
+
+
+def delete_task():
+    "Удаляет задачу"
+
+    pass
+
+
+def complete_task():
+    "Отмечает задачу как выполненную"
+
+    pass
+
+
+def complete_task():
+    "Отмечает задачу как невыполненную"
+
+    pass
+
+
+create_task()
