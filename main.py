89b95ea53d7586235770f1767c949bae995ad0f4
George <e.shegolihin2010@yandex.ru>
2026-03-10 22:24:33 +0300

    first steps

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-def say_hi():
-    print("Hello World")
+import json
+
+# with open("tasks.json", "r") as file:
+#     tasks = json.load(file)
+
+# tasks.append({"title": "Make a project", "is_done": False})
+
+# with open("tasks.json", "w") as file:
+#     json.dump(tasks, file, indent=4)
+
