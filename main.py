b97c77e7a5e2c588058c1c9ab08c6ac6a27fcda2
George <e.shegolihin2010@yandex.ru>
2026-03-15 17:50:03 +0300

    before optimization

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json, time
 with open("tasks.json", "r") as file:
     tasks = json.load(file)
 
+
 def main():
     completed_tasks = []
     incompleted_tasks = []
@@ -18,10 +19,7 @@ def main():
     def show_tasks():
         "Показывает задачи в списке"
 
-        print(
-            "\n--------------\n"
-            "\nAll my tasks:\n"
-            )
+        print("\n--------------\n" "\nAll my tasks:\n")
 
         if len(tasks) > 0:
 
@@ -42,29 +40,204 @@ def main():
                 "'v' - mark all your tasks as 'complete'\n"
                 "'x' - mark all your tasks as 'incomplete'\n"
                 "'add' - add a new task\n"
-                )
+            )
 
         else:
             print("You have no tasks yet\n")
 
             print("\n--------------\n")
 
-            print(
-                "'0' - main menu\n"
-                "'add' - add a new task\n"
-                )
+            print("'menu' - main menu\n" "'add' - add a new task\n")
 
-        user_choice = input("Print your command: ").strip()
-        
+        user_choice = input("Print your command: ").strip().lower()
+
         if user_choice.isdigit():
-            
-            num = int(user_choice)
-            
+
+            num = int(user_choice.strip())
+
             if 1 <= num <= len(tasks):
-                show_task_menu(tasks, num-1)
-            
+                show_task_menu(tasks, num - 1)
+
             else:
-                print("There's no task task with this number")
+                print(
+                    "\n--------------\n"
+                    "There's no task task with this number"
+                    "\n--------------\n"
+                )
+                time.sleep(2)
+                show_tasks()
+
+        else:
+
+            match user_choice:
+
+                case "menu":
+                    print("\n--------------\n")
+                    main()
+
+                case "add":
+                    create_task()
+
+                case "v":
+
+                    for task in tasks:
+                        task["is_done"] = True
+
+                    with open("tasks.json", "w", encoding="utf-8") as file:
+                        json.dump(tasks, file, ensure_ascii=False, indent=4)
+
+                    print(
+                        "\n--------------\n"
+                        "All tasks have been completed"
+                        "\n--------------\n"
+                    )
+                    time.sleep(2)
+                    show_tasks()
+
+                case "x":
+
+                    for task in tasks:
+                        task["is_done"] = False
+
+                    with open("tasks.json", "w", encoding="utf-8") as file:
+                        json.dump(tasks, file, ensure_ascii=False, indent=4)
+
+                    print(
+                        "\n--------------\n"
+                        "All tasks have been incompleted"
+                        "\n--------------\n"
+                    )
+                    time.sleep(2)
+                    show_tasks()
+
+                case "del":
+                    print(
+                        "\n--------------\n"
+                        "\nAre you sure?\n"
+                        "'1' - Yes\n"
+                        "'2' - No"
+                        "\n--------------\n"
+                    )
+
+                    sub_choice = input("Print your command: ").strip().lower()
+
+                    if sub_choice == "1":
+                        tasks.clear()
+
+                        with open("tasks.json", "w", encoding="utf-8") as file:
+                            json.dump(tasks, file, ensure_ascii=False, indent=4)
+
+                        print(
+                            "\n--------------\n"
+                            "All tasks have been deleted"
+                            "\n--------------\n"
+                        )
+                        time.sleep(2)
+                        main()
+
+                    elif sub_choice == "2":
+                        show_tasks()
+
+                    elif sub_choice == "":
+                        print("\n--------------\n" "Try again" "\n--------------\n")
+                        time.sleep(1)
+                        show_tasks()
+
+                case "":
+                    print("\n--------------\n" "Try again" "\n--------------\n")
+                    time.sleep(2)
+                    show_tasks()
+
+                case _:
+                    print("\n--------------\n" "Try again" "\n--------------\n")
+                    time.sleep(2)
+                    show_tasks()
+
+    def show_task_menu(tasks, index):
+        "Открывает подменю выбранной задачи"
+
+        task = tasks[index]
+
+        if task["is_done"] == True:
+            print(f"[x] {task['title']}")
+
+        else:
+            print(f"[ ] {task['title']}")
+
+        print(
+            "\n--------------\n"
+            "\nPress '1' to rename the task\n"
+            "Press '2' to delete the task\n"
+            "Press '3' to change complete status of the task\n"
+            "Press '4' to return\n"
+        )
+
+        user_choice = input("Print your number: ").strip().lower()
+
+        match user_choice:
+
+            case "1":
+                rename_task(index)
+                time.sleep(2)
+                show_tasks()
+
+            case "2":
+                delete_task(index)
+                time.sleep(2)
+                show_tasks()
+
+            case "3":
+                change_complete_status(index)
+                time.sleep(2)
+                show_tasks()
+
+            case "4":
+                show_tasks()
+
+            case "":
+                print("\n--------------\n" "Try again" "\n--------------\n")
+                time.sleep(2)
+                show_task_menu(tasks, index)
+
+    def show_completed_tasks():
+        "Показывает выполненные задачи"
+
+        if len(completed_tasks) > 0:
+            print("\nCompleted tasks:\n")
+
+            for i, task in enumerate(completed_tasks, start=1):
+                print(f"{i}. [x] {task['title']}")
+        else:
+            print("\nYou have no completed tasks\n" "\n--------------\n")
+
+            time.sleep(2)
+            main()
+
+        print("\n--------------\n")
+
+        print(
+            "'task number' - choose the task\n"
+            "'menu' - main menu\n"
+            "'del' - delete all completed tasks\n"
+            "'x' - mark all your tasks as 'incomplete'\n"
+            "'add' - add a new task\n"
+        )
+
+        user_choice = input("Print your command: ").strip().lower()
+
+        if user_choice.isdigit():
+
+            if 1 <= int(user_choice) <= len(completed_tasks):
+                print("sth")
+
+            else:
+                print(
+                    "\n--------------\n"
+                    "There's no task task with this number"
+                    "\n--------------\n"
+                )
+                time.sleep(2)
+                show_completed_tasks()
 
         else:
 
@@ -78,116 +251,42 @@ def main():
                     create_task()
 
                 case "del":
+                    tasks[:] = [task for task in tasks if not task["is_done"]]
+
+                    with open("tasks.json", "w", encoding="utf-8") as file:
+                        json.dump(tasks, file, ensure_ascii=False, indent=4)
+
                     print(
                         "\n--------------\n"
-                        "\nAre you sure?\n"
-                        "\n'1' - Yes\n"
-                        "\n'2' - No\n"
+                        "All completed tasks have been deleted"
                         "\n--------------\n"
-                        )
-
-                    sub_choice = input("Print your command: ")
-
-                    if sub_choice == "1":
-                        tasks.clear()
-                        print(
-                            "\n--------------\n"
-                            "\nAll tasks have been deleted\n"
-                            "\n--------------\n"
-                        )
-                        time.sleep(2)
-                        main()
-
-                    elif sub_choice == "2":
-                        show_tasks()
-
-                case _:
-                    print(
-                        "\n--------------\n" 
-                        "Try again" 
-                        "\n--------------\n"
-                        )
-                    time.sleep(1)
-                    show_tasks()
-
-    def show_task_menu(tasks, index):
-        "Открывает подменю выбранной задачи"
-
-        task = tasks[index]
-        
-        if task["is_done"] == True:
-            print(f"[x] {task['title']}")
-        
-        else:
-            print(f"[ ] {task['title']}")
-        
-        print(
-            "\n--------------\n"
-            "\nPress '1' to rename the task\n"
-            "Press '2' to delete the task\n"
-            "Press '3' to change complete status of the task\n"
-            "Press '4' to return\n"
-        )
-        
-        user_choice = input("Print your number: ")
-        
-        if user_choice == "2":
-            delete_task(index)
-            time.sleep(2)
-            show_tasks()
-    
-    def show_completed_tasks():
-        "Показывает выполненные задачи"
-
-        if len(completed_tasks) > 0:
-            print("\nCompleted tasks:\n")
-
-            for i, task in enumerate(completed_tasks, start=1):
-                print(f"{i}. [x] {task['title']}")
-        else:
-            print(
-                "\nYou have no completed tasks\n" 
-                "\n--------------\n"
-                )
-
-            time.sleep(3)
-            main()
-
-        print("\n--------------\n")
-
-        print(
-            "'task number' - choose the task\n"
-            "'menu' - main menu\n"
-            "'del' - delete all completed tasks\n"
-            "'x' - mark all your tasks as 'incomplete'\n"
-            "'add' - add a new task\n"
-        )
-
-        user_choice = input("Print your command: ").strip()
-        
-        if user_choice.isdigit():
-            
-            if 1 <= int(user_choice) <= len(completed_tasks):
-                print("sth")
-                
-        else:
-        
-            match user_choice:
-
-                case "menu":
-                    print("\n--------------\n")
+                    )
+                    time.sleep(2)
                     main()
 
-                case "add":
-                    create_task()
+                case "x":
+                    for task in tasks:
+                        task["is_done"] = False
+
+                    with open("tasks.json", "w", encoding="utf-8") as file:
+                        json.dump(tasks, file, ensure_ascii=False, indent=4)
+
+                    print(
+                        "\n--------------\n"
+                        "Status has been changed"
+                        "\n--------------\n"
+                    )
+                    time.sleep(2)
+                    main()
+
+                case "":
+                    print("\n--------------\n" "Try again" "\n--------------\n")
+                    time.sleep(2)
+                    show_completed_tasks()
 
                 case _:
-                    print(
-                        "\n--------------\n" 
-                        "Try again" 
-                        "\n--------------\n"
-                        )
-                    time.sleep(1)
+                    print("\n--------------\n" "Try again" "\n--------------\n")
+                    time.sleep(2)
                     show_completed_tasks()
 
         print("\n--------------\n")
@@ -201,11 +300,8 @@ def main():
             for i, task in enumerate(incompleted_tasks, start=1):
                 print(f"{i}. [ ] {task['title']}")
         else:
-            print(
-                "You have no incompleted tasks\n" 
-                "\n--------------\n"
-                )
-            time.sleep(3)
+            print("You have no incompleted tasks\n" "\n--------------\n")
+            time.sleep(2)
             main()
 
         print("\n--------------\n")
@@ -214,11 +310,11 @@ def main():
             "'task number' - choose the task\n"
             "'0' - main menu\n"
             "'del' - delete all incompleted tasks\n"
-            "'x' - mark all your tasks as 'complete'\n"
+            "'v' - mark all your tasks as 'complete'\n"
             "'add' - add a new task\n"
         )
 
-        user_choice = input("Print your command: ")
+        user_choice = input("Print your command: ").strip().lower()
 
         match user_choice:
 
@@ -229,13 +325,41 @@ def main():
             case "add":
                 create_task()
 
-            case _:
+            case "v":
+                for task in tasks:
+                    task["is_done"] = True
+
+                with open("tasks.json", "w", encoding="utf-8") as file:
+                    json.dump(tasks, file, ensure_ascii=False, indent=4)
+
                 print(
-                    "\n--------------\n" 
-                    "Try again" 
+                    "\n--------------\n" "Status has been changed" "\n--------------\n"
+                )
+                time.sleep(2)
+                main()
+
+            case "del":
+                tasks[:] = [task for task in tasks if task["is_done"]]
+
+                with open("tasks.json", "w", encoding="utf-8") as file:
+                    json.dump(tasks, file, ensure_ascii=False, indent=4)
+
+                print(
                     "\n--------------\n"
-                    )
-                time.sleep(1)
+                    "All incompleted tasks have been deleted"
+                    "\n--------------\n"
+                )
+                time.sleep(2)
+                main()
+
+            case "":
+                print("\n--------------\n" "Try again" "\n--------------\n")
+                time.sleep(2)
+                show_incompleted_tasks()
+
+            case _:
+                print("\n--------------\n" "Try again" "\n--------------\n")
+                time.sleep(2)
                 show_incompleted_tasks()
 
     def create_task():
@@ -243,20 +367,43 @@ def main():
 
         print("\n--------------\n")
 
-        new_task = input("Введите новую задачу: ")
-        tasks.append({"title": new_task, "is_done": False})
+        new_task = input("Print your new task: ").strip()
 
-        with open("tasks.json", "w") as file:
-            json.dump(tasks, file, indent=4)
+        if new_task != "":
+            tasks.append({"title": new_task, "is_done": False})
 
-        print(
-            "\n--------------\n" 
-            "\nA new task has been added\n" 
-            "\n--------------\n"
-            )
+            with open("tasks.json", "w") as file:
+                json.dump(tasks, file, indent=4)
+
+        else:
+            print("\n--------------\n" "Try again" "\n--------------\n")
+            time.sleep(2)
+            create_task()
+
+        print("\n--------------\n" "\nA new task has been added\n" "\n--------------\n")
 
         time.sleep(2)
         show_tasks()
+
+    def rename_task(index):
+        "Переименовывает задачу"
+
+        print("\n--------------\n")
+        new_name = input("Print a new name: ").strip()
+
+        if new_name != "":
+            tasks[index]["title"] = new_name
+
+            with open("tasks.json", "w", encoding="utf-8") as file:
+                json.dump(tasks, file, ensure_ascii=False, indent=4)
+
+        else:
+
+            print("\n--------------\n" "Try again" "\n--------------\n")
+            time.sleep(2)
+            rename_task(index)
+
+        print("\n--------------\n" "The name has been changed" "\n--------------\n")
 
     def delete_task(index):
         "Удаляет задачу"
@@ -267,39 +414,39 @@ def main():
             "\n'1' - Yes\n"
             "'2' - No\n"
             "\n--------------\n"
-            )
+        )
 
-        sub_choice = input("Print your command: ")
+        sub_choice = input("Print your command: ").strip().lower()
 
         if sub_choice == "1":
-            
+
             del tasks[index]
-            print(
-                "\n--------------\n"
-                "\nThe task has been deleted\n"
-                "\n--------------\n"
-            )
-            
+            print("\n--------------\n" "The task has been deleted" "\n--------------\n")
+
             with open("tasks.json", "w", encoding="utf-8") as file:
                 json.dump(tasks, file, ensure_ascii=False, indent=4)
 
         elif sub_choice == "2":
             show_tasks()
-        
+
         else:
             print("Try again")
-            time.sleep(1)
+            time.sleep(2)
             delete_task(task)
 
-    def complete_task():
-        "Отмечает задачу как выполненную"
+    def change_complete_status(index):
+        "Отмечает задачу как выполненную / невыполненную"
 
-        pass
+        if tasks[index]["is_done"] == True:
+            tasks[index]["is_done"] = False
 
-    def incomplete_task():
-        "Отмечает задачу как невыполненную"
+        elif tasks[index]["is_done"] == False:
+            tasks[index]["is_done"] = True
 
-        pass
+        with open("tasks.json", "w", encoding="utf-8") as file:
+            json.dump(tasks, file, ensure_ascii=False, indent=4)
+
+        print("\n--------------\n" "Status has been changed" "\n--------------\n")
 
     def show_menu():
 
@@ -313,7 +460,7 @@ def main():
             "Press '5' to exit\n"
         )
 
-        user_choice = input("Print your number: ")
+        user_choice = input("Print your number: ").strip().lower()
 
         print("\n--------------\n")
 
@@ -332,20 +479,20 @@ def main():
                 create_task()
 
             case "5":
-                print(
-                    "\n--------------\n"
-                    "\nGoodbye!\n"
-                    )
+                print("\n--------------\n" "\nGoodbye!\n")
                 return
 
+            case "":
+                print("Try again" "\n--------------\n")
+                time.sleep(2)
+                main()
+
             case _:
-                print(
-                    "Try again" 
-                    "\n--------------\n"
-                    )
-                time.sleep(1)
+                print("Try again" "\n--------------\n")
+                time.sleep(2)
                 main()
 
     show_menu()
+
 
 main()
